d37813e7727a55977867c95b6fd17910176a41b5
neerajvashistha <neerajvash8@gmail.com>
2020-02-22 20:42:56 +0000

    srcnn final code

--- DL_CV/CW1/CW1_Handout_Template_code/tf-SRCNN/srcnn.py
+++ DL_CV/CW1/CW1_Handout_Template_code/tf-SRCNN/srcnn.py
@@ -6,6 +6,8 @@ import numpy as np
 import tensorflow as tf
 import scipy
 import pdb
+from skimage import measure
+import skimage
 
 
 def imread(path, is_grayscale=True):
@@ -78,15 +80,15 @@ inputs = tf.placeholder(tf.float32, [None, input_size, input_size, c_dim], name=
 # conv2 layer with biases and relu: 32 filters with size 1 x 1
 # conv3 layer with biases and NO relu: 1 filter with size 5 x 5
 weights = {
-    'w1': tf.Variable(tf.random_normal([0, 0, 0, 0], stddev=1e-3), name='w1'),
-    'w2': tf.Variable(tf.random_normal([0, 0, 0, 0], stddev=1e-3), name='w2'),
-    'w3': tf.Variable(tf.random_normal([0, 0, 0, 0], stddev=1e-3), name='w3')
+    'w1': tf.Variable(tf.random_normal([9, 9, 1, 64], stddev=1e-3), name='w1'),
+    'w2': tf.Variable(tf.random_normal([1, 1, 64, 32], stddev=1e-3), name='w2'),
+    'w3': tf.Variable(tf.random_normal([5, 5, 32, 1], stddev=1e-3), name='w3')
     }
 
 biases = {
-      'b1': tf.Variable(tf.zeros([0]), name='b1'),
-      'b2': tf.Variable(tf.zeros([0]), name='b2'),
-      'b3': tf.Variable(tf.zeros([0]), name='b3')
+      'b1': tf.Variable(tf.zeros([64]), name='b1'),
+      'b2': tf.Variable(tf.zeros([32]), name='b2'),
+      'b3': tf.Variable(tf.zeros([1]), name='b3')
     }
 
 """Define the model layers with three convolutional layers
@@ -95,14 +97,14 @@ biases = {
 # replace 'None' with your layers: use the tf.nn.conv2d() and tf.nn.relu()
 # conv1 layer with biases and relu : 64 filters with size 9 x 9
 
-conv1 = None
+conv1 = tf.nn.relu(tf.nn.conv2d(inputs, weights['w1'], strides=[1,1,1,1], padding='VALID') + biases['b1'])
 ##------ Add your code here: to compute non-linear mapping
 # conv2 layer with biases and relu: 32 filters with size 1 x 1
 
-conv2 = None
+conv2 = tf.nn.relu(tf.nn.conv2d(conv1, weights['w2'], strides=[1,1,1,1], padding='VALID') + biases['b2'])
 ##------ Add your code here: compute the reconstruction of high-resolution image
 # conv3 layer with biases and NO relu: 1 filter with size 5 x 5
-conv3 = None
+conv3 = tf.nn.conv2d(conv2, weights['w3'], strides=[1,1,1,1], padding='VALID') + biases['b3']
 
 
 """Load the pre-trained model file
@@ -113,6 +115,33 @@ model = np.load(model_path, encoding='latin1', allow_pickle=True).item()
 ##------ Add your code here: show the weights of model and try to visualisa
 # variabiles (w1, w2, w3)
 
+weight_w1 = model['w1']
+weight_w2 = model['w2']
+weight_w3 = model['w3']
+filter_w1 = model['w1'].shape[3]
+filter_w2 = model['w2'].shape[3]
+filter_w3 = model['w3'].shape[3]
+
+print(filter_w1,filter_w2,filter_w3)
+fig = plt.figure(figsize = (8,8))
+for i in range(filter_w1):
+  sub_image = fig.add_subplot(8, 8, i+1)
+  sub_image.imshow(weight_w1[:, :, 0, i] ,interpolation = None, cmap = 'gray')
+plt.show()
+fig.savefig('weight_w1_subplot.jpg')
+
+
+fig = plt.figure(figsize = (8,8))
+for i in range(filter_w2):
+  sub_image = fig.add_subplot(8, 8, i+1)
+  sub_image.imshow(weight_w2[:, :, 0, i] ,interpolation = None, cmap = 'gray')
+plt.show()
+fig.savefig('weight_w2_subplot.jpg')
+
+
+plt.imshow(weight_w3[:, :, 0, 0] ,interpolation = None, cmap = 'gray')
+plt.show()
+plt.savefig('weight_w3_subplot.jpg')
 
 
 """Initialize the model variabiles (w1, w2, w3, b1, b2, b3) with the pre-trained model file
@@ -137,8 +166,20 @@ input_ = np.expand_dims(np.expand_dims(blurred_image, axis =0), axis=-1)
 
 # run the session
 # here you can also run to get feature map like 'conv1' and 'conv2'
-ouput_ = sess.run(conv3, feed_dict={inputs: input_})
+output_ = sess.run(conv3, feed_dict={inputs: input_})
 
 ##------ Add your code here: save the blurred and SR images and compute the psnr
 # hints: use the 'scipy.misc.imsave()'  and ' skimage.meause.compare_psnr()'
+scipy.misc.imsave('SR.jpg',output_[0,:,:,0])
+scipy.misc.imsave('ground_truth.jpg',output_[0,:,:,0])
+scipy.misc.imsave('blurred.jpg',blurred_image)
 
+groudtruth_image_cropped = groudtruth_image[6:-6,6:-6] 
+blurred_image_cropped = blurred_image[6:-6,6:-6]
+sr_float = output_[0,:,:,0].astype(np.float64)
+
+psnr_sr_br = skimage.measure.compare_psnr(groudtruth_image_cropped,blurred_image_cropped)
+psnr_sr_gt = skimage.measure.compare_psnr(groudtruth_image_cropped,sr_float)
+
+print("blurred image PSNR", str(psnr_sr_br))
+print("SR image PSNR", str(psnr_sr_gt))
